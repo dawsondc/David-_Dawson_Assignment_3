6150793418734f6e3e5cd0dd033070b1f669f423
David-C-Dawson <98625171+David-C-Dawson@users.noreply.github.com>
2022-03-29 18:56:47 -0400

    display names changed added html formatting

--- Models/Entities/Game.cs
+++ Models/Entities/Game.cs
@@ -1,7 +1,10 @@
-﻿namespace David__Dawson_Assignment_3.Models.Entities
+﻿using System.ComponentModel;
+
+namespace David__Dawson_Assignment_3.Models.Entities
 {
     public class Game
     {
+        [DisplayName("ID")]
         public int gameID { get; set; }
         public string Name { get; set; } = String.Empty;
         public string Genre  { get; set; } = String.Empty;
--- Models/Entities/Person.cs
+++ Models/Entities/Person.cs
@@ -1,9 +1,14 @@
-﻿namespace David__Dawson_Assignment_3.Models.Entities
+﻿using System.ComponentModel;
+
+namespace David__Dawson_Assignment_3.Models.Entities
 {
     public class Person
     {
+        [DisplayName("ID")]
         public int personID { get; set; }
+        [DisplayName("First Name")]
         public string FirstName { get; set; } = String.Empty;
+        [DisplayName("Last Name")]
         public string LastName { get; set; } = String.Empty;
 
 
